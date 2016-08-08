0481b264c16e1f560d05f35c959797f47491b63c
Yibo Kong <kyb11111@126.com>
2016-08-08 14:26:14 +0800

    HeapSort

---
+++ AlgorithmPython/AlgorithmPython/HeapSort.py
@@ -0,0 +1,35 @@
+import random
+
+Left = lambda i: 2*i
+Right = lambda i: 2*i + 1
+Parent = lambda i: i/2
+
+Array = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+
+def MaxHeapify(A, i, heapsize):
+    l = Left(i)
+    if l < heapsize and A[l] > A[i]:
+        largest = l
+    else:
+        largest = i
+    r = Right(i)
+    if r < heapsize and A[r] > A[largest]:
+        largest = r
+    if largest != i:
+        A[i],A[largest] = A[largest],A[i]
+        MaxHeapify(A, largest, heapsize)
+      
+def BuildMaxHeap(A):
+    ArrayLen = len(A)
+    for i in range(ArrayLen/2, -1, -1):
+        MaxHeapify(A, i, ArrayLen)
+
+def HeapSort(A):
+    BuildMaxHeap(A)
+    heapsize = len(A)
+    for i in range(heapsize - 1, 0, -1):
+        A[0],A[i] = A[i],A[0]
+        MaxHeapify(A, 0, i)
+
+HeapSort(Array)
+print Array
